195f00e133586ea8dcf595d71171e0132243ff7a
Alin528491 <alin@alinalin.net>
2026-06-06 19:36:50 +0700

    Initial Commit

---
+++ KneeHab Pro/KneeHab_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  KneeHab_ProApp.swift
+//  KneeHab Pro
+//
+//  Created by 20532 on 6/6/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct KneeHab_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
